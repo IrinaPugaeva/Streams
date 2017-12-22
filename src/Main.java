939f916354fbeda67bb6ba7638e485b2261b9cf9
IrinaPugaeva <pugaevaarina@gmail.com>
2017-12-22 23:16:31 +0300

    Stream

---
+++ src/Main.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) {
+        Person p1 = new Person("Alex", 20);
+        Person p2 = new Person("Bob", 35);
+        Person p3 = new Person("Alice", 15);
+        Person p4 = new Person("Max", 32);
+        Person p5 = new Person("Kimberly", 50);
+
+        List<Person> people = new ArrayList<>();
+
+        people.add(p1);
+        people.add(p2);
+        people.add(p3);
+        people.add(p4);
+        people.add(p5);
+
+        Map m1 = Streams.of(people)
+                .toMap(p -> p.getName(), p -> p.getAge());
+
+        System.out.println(m1);
+        System.out.println();
+        System.out.println("filter: age > 30");
+
+        Map m2 = Streams.of(people)
+                .filter(p -> p.getAge() > 30)
+                .toMap(pOver30 -> pOver30.getName(), pOver30 -> pOver30.getAge());
+
+        System.out.println(m2);
+        System.out.println();
+
+        System.out.println("increase all ages by 100");
+
+        Map m3 = Streams.of(people)
+                .transform(p -> new Person(p.getName(), p.getAge() + 100))
+                .toMap(p -> p.getName(), p -> p.getAge());
+        System.out.println(m3);
+    }
+}
